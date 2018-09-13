f5aeb384804b3d63f5a82759d807c0bf78c415a7
psh-suresh <sureshpraja1234@gmail.com>
2018-09-13 16:35:20 +0530

    First commit

---
+++ test/index.js
@@ -0,0 +1,8 @@
+var logger = require('../lib/logger');
+
+logger.warn('msg');
+logger.error('error');
+logger.info('info');
+logger.verbose('verbose');
+logger.debug('debug');
+logger.silly('silly');
